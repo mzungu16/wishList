7961ccee58470c39f866a76019519c267d48f00f
Gleb <gleb.s.smirnov@gmail.com>
2022-10-04 20:26:52 +0400

    добавил макет главного экрана

--- app/src/main/java/com/example/wishlist/MainActivity.kt
+++ app/src/main/java/com/example/wishlist/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.wishlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val fab = findViewById<FloatingActionButton>(R.id.main_fab)
+        fab.setOnClickListener {
+            fab.isExpanded = !fab.isExpanded
+        }
     }
 }
