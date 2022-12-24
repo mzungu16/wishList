a7e89fcb8feda19ade1167f4bceedac80c1e9a46
unknown <gleb.s.smirnov@gmail.com>
2022-12-24 22:09:20 +0400

    добавлен viewBinding

--- app/src/main/java/com/example/wishlist/MainActivity.kt
+++ app/src/main/java/com/example/wishlist/MainActivity.kt
@@ -3,14 +3,16 @@ package com.example.wishlist
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.example.wishlist.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by viewBinding(ActivityMainBinding::bind)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-
     }
 }
